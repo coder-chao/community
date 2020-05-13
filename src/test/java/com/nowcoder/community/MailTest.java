ec9735f7cab4198e356711678b642fd770566eb2
coder-chao <3243517453@qq.com>
2020-05-13 21:40:31 +0800

    first commit

---
+++ src/test/java/com/nowcoder/community/MailTest.java
@@ -0,0 +1,42 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.MailClient;
+import org.junit.Test;
+import org.junit.platform.engine.TestEngine;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTest {
+
+    @Autowired
+    private MailClient client;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testSendMail(){
+        client.sendMail("1131494062@qq.com","test","welcome");
+    }
+
+    @Test
+    public void testSendHtmlMail(){
+        Context context = new Context();
+        context.setVariable("username", "sunday");
+        String content = templateEngine.process("/mail/mailDemo", context);
+        System.out.println(content);
+        client.sendMail("a1131494062@163.com","html",content);
+
+    }
+
+}

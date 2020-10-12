f04c7272b192f8772997662e1e23548bdd272dd3
coder-chao <3243517453@qq.com>
2020-10-12 19:48:09 +0800

    完善注册功能

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,29 @@
+$(function(){
+	$("#publishBtn").click(publish);
+});
+
+function publish() {
+	$("#publishModal").modal("hide");
+	// 获取标题和内容
+	var title = $("#recipient-name").val();
+	var content = $("#message-text").val();
+	// 发送post请求
+	$.post(
+		CONTEXT_PATH + "/discuss/add",
+		{"title":title,"content":content},
+		function(data) {
+			data = $.parseJSON(data);
+			$("#hintBody").text(data.msg);
+			$("#hintModal").modal("show");
+			setTimeout(function(){
+				$("#hintModal").modal("hide");
+				if (data.code == 0){
+					window.location.reload();
+				}
+			}, 2000);
+		}
+
+	);
+
+
+}
